d847357dd40d5e510e8cb06f366fe08c217bf785
Upadhyay <prayag.upd@gmail.com>
2018-11-28 22:07:00 -0800

    update http4s 0.19.0

--- README.md
+++ README.md
@@ -9,12 +9,18 @@ java -jar target/scala-2.12/http4s-hello-server.jar
 
 ```bash
 curl -H "correlationId: 1234" localhost:8080/api/chat/history/prayagupd
-{"correlationId":"","history":["hi, how can i help you?","here is coffee shop"]}
+{
+"correlationId":"",
+"history":["hi, how can i help you?","here is coffee shop"]
+}
 ```
 
 ```bash
-$ curl -H "correlatioId: 1234" localhost:8080/api/chat/history/prayagupd
-{"correlationId":"","history":["hi, how can i help you?","here is coffee shop"]}
+curl -H "correlationId: 1234" -d '{"correlationId": "abc", "utterance": "some utterance"}' localhost:8080/api/chat
+{
+"correlationId":"abc",
+"displayText":"Here are near by coffee shops"
+}
 ```
 
 TODO
--- src/main/scala/server/RestServer.scala
+++ src/main/scala/server/RestServer.scala
@@ -1,22 +1,23 @@
 package server
 
-import cats.data.{Kleisli, OptionT}
+import cats.data.Kleisli
+import cats.effect._
+import cats.implicits._
 import cats.effect.IO
-import fs2.StreamApp
 import io.circe._
 import io.circe.generic.auto._
 import io.circe.syntax._
 import org.http4s._
 import org.http4s.circe._
-import org.http4s.dsl.Http4sDsl
-import org.http4s.server.blaze.BlazeBuilder
+import org.http4s.dsl.io._
+import org.http4s.server.blaze._
 import schema.{ChatHistory, ChatRequest, ChatResponse}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import org.http4s.rho.RhoService
 
-object RestServer extends StreamApp[IO] with Http4sDsl[IO] {
+object RestServer extends IOApp {
 
   implicit val chatRequestDecoder = jsonOf[IO, ChatRequest]
   implicit val chatResponseEncoder = jsonEncoderOf[IO, ChatResponse]
@@ -35,31 +36,34 @@ object RestServer extends StreamApp[IO] with Http4sDsl[IO] {
     }
   }
 
-  val service: HttpService[IO] = HttpService[IO] {
+  val service: Kleisli[IO, Request[IO], Response[IO]] = HttpRoutes.of[IO] {
 
     case req@GET -> Root / "api" / "chat" / "history" / (userId: String) =>
       val correlationId = req.headers.find(_.name == "correlationId").map(_.value).getOrElse("")
       val chatHistResponse: IO[Response[IO]] =
-        Ok(Future.successful(ChatHistory(
+        Ok(ChatHistory(
           correlationId,
-          List("hi, how can i help you?", "here is coffee shop")))
+          List("hi, how can i help you?", "here is coffee shop"))
         )
       chatHistResponse
 
     case request@POST -> Root / "api" / "chat" =>
       for {
         chatRequest <- request.as[ChatRequest]
-        chatResponse <- Ok(Future.successful(ChatResponse(
+        chatResponse <- Ok(ChatResponse(
           chatRequest.correlationId,
-          "Here are near by coffee shops"))
+          "Here are near by coffee shops")
         )
       } yield chatResponse
 
-  }
+  }.orNotFound
 
-  def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, StreamApp.ExitCode] =
-    BlazeBuilder[IO]
+  def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO]
       .bindHttp(8080, "0.0.0.0")
-      .mountService(service, "/")
+      .withHttpApp(service)
       .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
 }
